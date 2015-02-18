cd034c2fe01c2a55b370ad093e08472827fa8693
bychevoz <ivak_@mail.ru>
2015-02-18 20:10:26 +0200

    utils.py

--- Frequency.py
+++ Frequency.py
@@ -2,8 +2,23 @@ from utils import textfromfile
 
 class FrequencyAnalyzer:
     def __init__(self):
-        pass
+        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.frequencies = dict.fromkeys(self.alphabet, 0)
+        self.text = ""
+        self.len = 0
 
-text = textfromfile("bigclean")
-print(text[0:20])
+    def feed(self, filename):
+        self.frequencies = dict.fromkeys(self.alphabet, 0)
+        self.text = textfromfile(filename)
+        self.len = len(self.text)
+        for s in self.text:
+            self.frequencies[s] += 1
 
+    def get_freq(self):
+        return sorted(self.frequencies.items(), key=lambda s: s[1], reverse=True)
+
+    def get_len(self):
+        return self.len
+
+analyzer = FrequencyAnalyzer()
+analyzer.feed("bigclean")
